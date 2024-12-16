9996fb3e92ed3d4dfc73ea061b13aedaae74ae54
koo21 <sawaddiju@hotmail.com>
2024-12-16 09:46:02 +0700

    aek-up

--- components/layoutFooter.php
+++ components/layoutFooter.php
@@ -1,3 +1,16 @@
+<div class="container-fit w-auto mx-auto bg-slate-100 h-auto py-10 mt-12">
+    <div class="container text-center">
+        <div class="">
+            <div class="mx-auto text-center"><img src="../storage/images/bannerHome-3.png" alt="" srcset=""
+                    class="w-20 mx-auto"></div>
+            <div class="mt-2 text-ms">© 2024 TeedinDD</div>
+            <div class=" text-sm">ติดต่อเราได้ที่</div>
+            <div class="text-sm">teedindd.com <i class="bi bi-phone"></i> 087-4995692 e-mail: teedindd@gmail.com</div>
+        </div>
+
+    </div>
+</div>
+
 <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
